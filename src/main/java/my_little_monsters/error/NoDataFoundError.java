b8fdf014a316c5b493123af69371c6f13c9cfa5d
AgathePons <ponsagathe37@gmail.com>
2022-11-27 16:01:21 +0100

    update: no data found error ok

--- src/main/java/my_little_monsters/error/NoDataFoundError.java
+++ src/main/java/my_little_monsters/error/NoDataFoundError.java
@@ -1,4 +1,12 @@
 package my_little_monsters.error;
 
-public class NoDataFoundError {
+public class NoDataFoundError extends RuntimeException {
+    public NoDataFoundError(String message) {
+        super(message);
+    }
+
+    public static NoDataFoundError withId(String itemType, int id) {
+        return new NoDataFoundError(
+                itemType +" with id" + id + " not found");
+    }
 }
