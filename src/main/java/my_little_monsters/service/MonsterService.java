ca0605e1c4038f7a446980732da6c6e8b039f87c
AgathePons <ponsagathe37@gmail.com>
2022-12-03 15:51:35 +0100

    update: refactor monster service interface and implementation

--- src/main/java/my_little_monsters/service/MonsterService.java
+++ src/main/java/my_little_monsters/service/MonsterService.java
@@ -1,21 +1,22 @@
 package my_little_monsters.service;
 
 import my_little_monsters.dto.MonsterDto;
+import my_little_monsters.entities.Monster;
 
 import java.util.Optional;
 import java.util.Set;
 
 public interface MonsterService {
 
-    Set<MonsterDto> findAll();
+    Iterable<Monster> findAll();
 
-    Optional<MonsterDto> findById(int id);
+    Optional<Monster> findById(int id);
 
-    Set<MonsterDto> searchQuery(String name, Double attack, Double defense, boolean isCute);
+    Iterable<MonsterDto> searchQuery(String name, Double attack, Double defense, boolean isCute);
 
-    MonsterDto add(MonsterDto monsterDto);
+    Monster add(MonsterDto monsterDto);
 
-    Optional<MonsterDto> update(MonsterDto monsterDto);
+    Optional<Monster> update(MonsterDto monsterDto);
 
     boolean delete(int id);
 }
--- src/main/java/my_little_monsters/service/impl/MonsterServiceImpl.java
+++ src/main/java/my_little_monsters/service/impl/MonsterServiceImpl.java
@@ -1,107 +1,58 @@
 package my_little_monsters.service.impl;
 
 import my_little_monsters.dto.MonsterDto;
+import my_little_monsters.entities.Monster;
+import my_little_monsters.repository.MonsterRepository;
 import my_little_monsters.service.MonsterService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 import java.util.Set;
 
 public class MonsterServiceImpl implements MonsterService {
+
+    @Autowired
+    private MonsterRepository monsterRepository;
+
     @Override
-    public Set<MonsterDto> findAll() {
-        return Set.of(
-                MonsterDto.builder()
-                        .id(1)
-                        .name("Boogie Man")
-                        .attackFactor(0.30)
-                        .defenseFactor(0.10)
-                        .isCute(false)
-                        .build(),
-                MonsterDto.builder()
-                        .id(2)
-                        .name("Mush mush")
-                        .attackFactor(0.20)
-                        .defenseFactor(0.50)
-                        .isCute(true)
-                        .build(),
-                MonsterDto.builder()
-                        .id(3)
-                        .name("Spider Queen")
-                        .attackFactor(0.70)
-                        .defenseFactor(0.20)
-                        .isCute(false)
-                        .build()
-        );
+    public Iterable<Monster> findAll() {
+        return this.monsterRepository.findAll();
     }
 
     @Override
-    public Optional<MonsterDto> findById(int id) {
-        if (id == 0) {
-            return Optional.empty();
-        } else {
-            return Optional.of(MonsterDto.builder()
-                    .id(3)
-                    .name("Spider Queen")
-                    .attackFactor(0.70)
-                    .defenseFactor(0.20)
-                    .isCute(false)
-                    .build());
+    public Optional<Monster> findById(int id) {
+        return this.monsterRepository.findById(id);
+    }
+
+    @Override
+    public Monster add(MonsterDto monsterDto) {
+        return this.monsterRepository.save(monsterDto.toMonsterEntity());
+    }
+
+    @Override
+    public Optional<Monster> update(MonsterDto monsterDto) {
+        Monster monsterEntity = monsterDto.toMonsterEntity();
+        Optional<Monster> oMonster = this.monsterRepository.findById(monsterEntity.getId());
+        if(oMonster.isPresent()) {
+            this.monsterRepository.save(monsterEntity);
+            return Optional.of(monsterEntity);
         }
-
-    }
-
-    @Override
-    public Set<MonsterDto> searchQuery(String name, Double attack, Double defense, boolean isCute) {
-        return Set.of(
-                MonsterDto.builder()
-                        .id(2)
-                        .name("Mush mush")
-                        .attackFactor(0.20)
-                        .defenseFactor(0.50)
-                        .isCute(true)
-                        .build(),
-                MonsterDto.builder()
-                        .id(3)
-                        .name("Spider Queen")
-                        .attackFactor(0.70)
-                        .defenseFactor(0.20)
-                        .isCute(false)
-                        .build()
-        );
-    }
-
-    @Override
-    public MonsterDto add(MonsterDto monsterDto) {
-        monsterDto.setId(666);
-        return monsterDto;
-    }
-
-    @Override
-    public Optional<MonsterDto> update(MonsterDto monsterDto) {
-        if (monsterDto.getId() == 0) {
-            return Optional.empty();
-        } else {
-            var monster = MonsterDto.builder()
-                    .name("Name")
-                    .attackFactor(0.50)
-                    .defenseFactor(0.50)
-                    .isCute(false)
-                    .build();
-            monster.setName(monsterDto.getName());
-            monster.setAttackFactor(monsterDto.getAttackFactor());
-            monster.setDefenseFactor(monsterDto.getDefenseFactor());
-            monster.setIsCute(monsterDto.getIsCute());
-            return Optional.of(monster);
-        }
+        return Optional.empty();
     }
 
     @Override
     public boolean delete(int id) {
-        if (id == 0) {
-            return false;
-        } else {
+        Optional<Monster> monsterToDelete = this.monsterRepository.findById(id);
+        if (monsterToDelete.isPresent()) {
+            this.monsterRepository.delete(monsterToDelete.get());
             return true;
         }
+        return false;
+    }
 
+    @Override
+    public Set<MonsterDto> searchQuery(String name, Double attack, Double defense, boolean isCute) {
+        // TODO
+        return null;
     }
 }
